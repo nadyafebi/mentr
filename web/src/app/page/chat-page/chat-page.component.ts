8b9f3a21cbc1ea9a930b6c0261f543cb8e89100e
Nadya Djojosantoso <hello@nadyafebi.com>
2020-02-02 00:04:16 -0800

    Update chat

--- web/src/app/page/chat-page/chat-page.component.ts
+++ web/src/app/page/chat-page/chat-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { DataService, UserService } from '../../services';
-import { Chat } from '../../schemas';
+import { Chat, Match } from '../../schemas';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -17,6 +17,7 @@ export class ChatPageComponent implements OnInit {
   userId: string;
   chatroomId: string;
   chats$: Observable<Chat[]>;
+  match$: Observable<Match>;
   chatForm = new FormControl('');
 
   constructor(
@@ -29,6 +30,7 @@ export class ChatPageComponent implements OnInit {
     this.userId = this.userService.getUserId();
     this.chatroomId = this.route.snapshot.paramMap.get('chatroomId');
     this.chats$ = this.dataService.getChats$(this.chatroomId);
+    this.match$ = this.dataService.getDoc$('matches', this.chatroomId);
   }
 
   async sendText() {
